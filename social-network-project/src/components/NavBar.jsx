88e7fc78435b2d39a5978277e87eeb735af916a1
María Macarena Alvarez Castillo <mariamacarenaac@gmail.com>
2023-08-08 10:08:16 -0300

    working on tanslations and dark mode on the navbar and sidebar

--- social-network-project/src/components/NavBar.jsx
+++ social-network-project/src/components/NavBar.jsx
@@ -1,9 +1,12 @@
 import { RiSearchLine, RiContrastFill, RiEmphasisCn, RiMenuLine } from "react-icons/ri";
-import { useState } from "react";
+import { useState, useContext} from "react";
 import useAuthStore from '../store/useAuthStore'
 import SideBar from "./SideBar";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Context } from "../context/Context";
+import { useTranslate } from "../hooks/useTranslate";
+import { Translations } from "../translations/translations";
 
 
 const NavBar = () => {
@@ -11,6 +14,13 @@ const NavBar = () => {
     const logout = useAuthStore((state) => state.logout);
     const [filterUser, setFilterUser] = useState('');
     const navigate = useNavigate()
+    const context = useContext(Context);
+    const translations = useTranslate(Translations(context));
+    const themeBackground = context.clearTheme ? "bg-black-100" : "bg-[#333333]";
+    const textColor = context.clearTheme ? "600" : "100"
+    const navbarHoverButtonBackground = context.clearTheme ? "hover:bg-[#ffffff]" : "hover:bg-gray-300 hover:text-black-600" ;
+
+
 
     const toggleMenu = () => {
         setShowMenu(!showMenu);
@@ -52,19 +62,20 @@ const NavBar = () => {
 
     return (
         <>
-            <nav className="bg-black-100 w-full fixed top-0 h-14 text-center flex justify-between items-center">
+            <nav className={`${themeBackground} w-full fixed top-0 h-14 text-center flex justify-between items-center`}
+      >
                 <h1 className=" w-20 hidden lg:block">LOGO</h1>
                 <button onClick={toggleMenu} className={`md:hidden text-4xl ml-5 text-black-600`}><RiMenuLine /></button>
                 <form className=" relative w-1/2">
-                    <input type="text" placeholder="Buscar usuario"
+                    <input type="text" placeholder={translations.searchUser}
                         className=" rounded-full py-2 px-4 pr-10 focus:outline-none focus:border-black-500 w-full"
                         onChange={handleChange} value={filterUser} />
                     <button className="absolute right-0 top-1 mt-2 mr-3" onClick={handleSubmit}><RiSearchLine /></button>
                 </form>
                 <ul className="flex justify-end items-center w:2/3 md:w-1/3 ">
-                    <li className="text-4xl mr-4 text-black-600 hover:bg-[#ffffff] rounded-lg" ><a href="/" ><RiEmphasisCn /></a></li>
-                    <li className="text-4xl mr-4 text-black-600 hover:bg-[#ffffff] rounded-lg"><a href="/"><RiContrastFill /></a></li>
-                    <li><button className="mr-4 bg-[#25fc98] text-white hover:bg-[#15b575] px-4 py-2 rounded-lg shadow-md" onClick={handleLogout}>Cerrar sesión</button></li>
+                    <li className={`text-4xl mr-4 text-black-${textColor} ${navbarHoverButtonBackground} rounded-lg`} ><a href="/" ><RiEmphasisCn /></a></li>
+                    <li className={`text-4xl mr-4 text-black-${textColor} ${navbarHoverButtonBackground} rounded-lg`}><a href="/"><RiContrastFill /></a></li>
+                    <li><button className="mr-4 bg-[#25fc98] text-white hover:bg-[#15b575] px-4 py-2 rounded-lg shadow-md" onClick={handleLogout}>{translations.signOff}</button></li>
                     
 
                 </ul>
--- social-network-project/src/translations/translations.jsx
+++ social-network-project/src/translations/translations.jsx
@@ -3,13 +3,27 @@ export const Translations = (par) =>{
    
     if (par.language === 'en'){ return{
     birthday:"Birthday",
-    gender: "Gender"
+    gender: "Gender",
+    signOff:"Sign off",
+    searchUser: "Search user"
 }}
 
 if (par.language === 'es'){
    return {
     birthday:"Fecha de nacimiento",
-    gender:"Género"
+    gender:"Género",
+    signOff:"Cerrar Sesión",
+    searchUser: "Buscar usuario"
     }
 }
+
+if (par.language === 'fr') {
+    return {
+        birthday: "Date de naissance",
+        gender: "Genre",
+        signOff: "Déconnexion",
+        searchUser: "nose"
+    };
+}
+
 }
